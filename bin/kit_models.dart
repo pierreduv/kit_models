9c7e069894050b28eb3a7f9fcb1d14f2e554cb51
pierreduv <pierreduv@hotmail.com>
2024-03-11 16:28:08 -0400

    First commit

---
+++ bin/kit_models.dart
@@ -0,0 +1,5 @@
+import 'package:kit_models/kit_models.dart' as kit_models;
+
+void main(List<String> arguments) {
+  print('Hello world: ${kit_models.calculate()}!');
+}
---
+++ test/kit_models_test.dart
@@ -0,0 +1,8 @@
+import 'package:kit_models/kit_models.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
