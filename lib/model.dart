dede37f6776e36dbc9bffef0f8ebec6f11888f7a
pierreduv <pierreduv@hotmail.com>
2024-03-28 23:42:35 -0400

    v1

--- lib/model.dart
+++ lib/model.dart
@@ -4,6 +4,14 @@ typedef Json = Map<String,dynamic>;
 
 
 abstract class Model {
+  ModelCompanion? _companion;
+  ModelCompanion get companion {
+    _companion ??= newModelCompanion(this);
+    return _companion!;
+  }
+
+  ModelCompanion newModelCompanion(Model model);
+
   static void createItem(Model object, String tableName) {
   }
 }
